3d33af5107578372f7351e4ef98d89c4ce8bf318
Ailton Souza <ailton.95@hotmail.com>
2022-11-28 23:35:02 -0300

    ex5 retorna true se os angulos representarem um triangulo

---
+++ fundamentos/secao04-introducao-javascript/dia01-primeiros-passos/ex5.js
@@ -0,0 +1,14 @@
+const a1 = 60;
+const a2 = 0;
+const a3 = 60;
+
+
+if (a1+a2+a3 === 180){
+    console.log("True");
+}
+else if (a1 <= 0 || a2 <= 0 || a3 <= 0){
+    console.log("Error");
+}
+else {
+    console.log("False");
+}
