8317fcacba2dd6de8ab1d81f9e6d3f758780bb62
Ailton Souza <ailton.95@hotmail.com>
2022-11-28 23:42:07 -0300

    ex7 if/else - retorna porcentagem

---
+++ fundamentos/secao04-introducao-javascript/dia01-primeiros-passos/ex7.js
@@ -0,0 +1,23 @@
+const p = 50;
+
+if(p < 50 && p >= 0 ){
+    console.log("F");
+}
+else if(p >= 90 && p <= 100){
+    console.log("A");
+}
+else if(p >= 80 && p <= 100){
+    console.log("B");
+}
+else if(p >= 70 && p <= 100){
+    console.log("C");
+}
+else if(p >= 60 && p <= 100){
+    console.log("D");
+}
+else if(p >= 50 && p <= 100){
+    console.log("E");
+}
+else {
+    console.log("Erro");
+}
