f8344386a6b6b59c2fe306df6411a79373dc6ba5
Ailton Souza <ailton.95@hotmail.com>
2022-11-28 23:06:41 -0300

    ex3 retorna o maior numero entre três

--- fundamentos/secao04-introducao-javascript/dia01-primeiros-passos/ex2.js
+++ fundamentos/secao04-introducao-javascript/dia01-primeiros-passos/ex2.js
@@ -1,15 +1,10 @@
 const nmr1 = 20;
 const nmr2 = 30;
-const nmr3 = 10;
 
-if (nmr1 > nmr2 && nmr1 > nmr3){
+if (nmr1 > nmr2 ){
     console.log(nmr1);
 }
 
-else if (nmr2 > nmr1 && nmr2 > nmr3){
+else {
     console.log(nmr2);
-}
-
-else{
-    console.log(nmr3);
 }
---
+++ fundamentos/secao04-introducao-javascript/dia01-primeiros-passos/ex3.js
@@ -0,0 +1,15 @@
+const nmr1 = 20;
+const nmr2 = 30;
+const nmr3 = 20;
+
+if (nmr1 > nmr2 && nmr1 > nmr3){
+    console.log(nmr1);
+}
+
+else if (nmr2 > nmr1 && nmr2 > nmr3){
+    console.log(nmr2);
+}
+
+else{
+    console.log(nmr3);
+}
