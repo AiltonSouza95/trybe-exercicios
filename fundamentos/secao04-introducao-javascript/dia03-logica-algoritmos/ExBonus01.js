4872d2942fe6dd70a2f736b3fa44a92d960f8289
Ailton Souza <ailton.95@hotmail.com>
2022-11-30 19:44:54 -0300

    ExBonus01 Montando quadrado de caracteres

---
+++ fundamentos/secao04-introducao-javascript/dia03-logica-algoritmos/ExBonus01.js
@@ -0,0 +1,10 @@
+let nmr = 10;
+
+for (let index = 0; index < nmr; index += 1) {   
+    let char = '';
+    for (let index = 1; index <= nmr; index += 1){
+        char = char + "*";
+    }
+    console.log(char);
+}
+console.log();
