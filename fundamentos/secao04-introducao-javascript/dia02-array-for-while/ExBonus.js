2c312c604e80a4ee45b04fa8bd8b5c6f3c5d5b98
Ailton Souza <ailton.95@hotmail.com>
2022-11-30 12:26:58 -0300

    ExBonus1 Mostrando o array em ordem crescente

---
+++ fundamentos/secao04-introducao-javascript/dia02-array-for-while/ExBonus.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let marc = 0;
+
+for (i = 1; i < numbers.length; i += 1) {
+    for (let j = 0; j < numbers.length; j += 1) {
+      if (numbers[i] < numbers[j]) {
+        marc = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = marc;
+      }
+    }
+}
+console.log(numbers);
