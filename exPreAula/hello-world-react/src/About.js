bc56c79a5113f061c4e98c9eb6a84644ef59eac0
Ailton Souza <ailton.95@hotmail.com>
2023-04-19 08:17:15 -0300

    ex pre aula

---
+++ exPreAula/hello-world-react/src/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ailton Souza
+        </h1>
+        <p>
+          Olá, sou Ailton, estudando de Desenvolvimento Web na Trybe, e formado em Analise de Sistemas
+        </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaSscript</li>                    
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
